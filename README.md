58018eaa5de6b5784a7208d2b58fd46b165df561
Jiří Zmrhal <jzmrhal@actum.cz>
2024-03-18 15:23:08 +0100

    Initialize the front end

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mmr
+
 A project for Ministry Of Regional Development Of The Czech Republic
---
+++ front-end/app/page.tsx
@@ -0,0 +1,17 @@
+import { GovContainer, GovNav, GovNavItem } from "./design-system";
+
+export default function Home() {
+  return (
+    <div>
+      <GovContainer>
+        <GovNav>
+          <GovNavItem href="#">Test</GovNavItem>
+          <GovNavItem href="#">Test</GovNavItem>
+          <GovNavItem href="#">Test</GovNavItem>
+          <GovNavItem href="#">Test</GovNavItem>
+          <GovNavItem href="#">Test</GovNavItem>
+        </GovNav>
+      </GovContainer>
+    </div>
+  );
+}
