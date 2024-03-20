e44b08c634cb7699f6735ed3911091c41de3e53c
pavelhaj93 <pavel.hajduch@actumdigital.com>
2024-03-20 16:53:03 +0700

    intiial commit

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,17 +1,10 @@
-import { GovContainer, GovNav, GovNavItem } from "./design-system";
+import Button from "./_components/Button"
+import { GovContainer } from "./design-system"
 
 export default function Home() {
   return (
-    <div>
-      <GovContainer>
-        <GovNav>
-          <GovNavItem href="#">Test</GovNavItem>
-          <GovNavItem href="#">Test</GovNavItem>
-          <GovNavItem href="#">Test</GovNavItem>
-          <GovNavItem href="#">Test</GovNavItem>
-          <GovNavItem href="#">Test</GovNavItem>
-        </GovNav>
-      </GovContainer>
-    </div>
-  );
+    <GovContainer>
+      <Button>button</Button>
+    </GovContainer>
+  )
 }
